df7504350e74a576e5f29653878b2d0e91faabd0
luisbryandr <luisbdominguezr@gmail.com>
2021-08-11 02:11:30 -0400

    made my likepost function work in App js

--- src/App.js
+++ src/App.js
@@ -31,6 +31,9 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+      const newPosts = posts.map(post=> post.id === postId ? { ...post, likes: post.likes + 1} : post)
+      setPosts(newPosts)
   };
 
   return (
